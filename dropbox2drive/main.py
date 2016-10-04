bc492294b17b9c2a1a79acee1900a933848a9cd6
Josep <jap8@alumnes.udl.cat>
2016-10-04 12:56:02 +0300

    Implemented tests of comparing files and File class.

--- dropbox2drive/main.py
+++ dropbox2drive/main.py
@@ -1,12 +1,47 @@
 import googledrive
 import dropbox
+import filecmp
+import tempfile
+import os
 
 
 class File(object):
     """Representation of a file."""  # TODO
 
-    def __init__(self):
-        pass
+    def __init__(self,
+                 name,
+                 relative_path,
+                 cloud,
+                 original_file,
+                 last_modified,
+                 metadata=None,  # TODO, some common metadata structure
+                 ):
+        self.name = name
+        self.relative_path = relative_path
+        self.cloud = cloud
+        self.last_modified = last_modified
+        self.metadata = metadata
+        # if the dir doesn't exist for this cloud, create it
+        cloud_dir = os.path.join(tempfile.gettempdir(), cloud['tmp_dir'])
+        if not os.path.exists(cloud_dir):
+            os.makedirs(cloud_dir)
+        self.tmp_file = tempfile.NamedTemporaryFile(dir=cloud_dir)
+        self.tmp_file.write(open(original_file, 'rb').read())
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return (
+                     self.name == other.name and
+                     self.relative_path == other.relative_path and
+                     self.last_modified == other.last_modified and
+                     self.metadata.__eq__(other.metadata) and
+                     filecmp.cmp(self.tmp_file.name, other.tmp_file.name)
+                    )
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
 
 def classify_files(new_file_index, last_file_index):
--- dropbox2drive/tests/test_main_methods.py
+++ dropbox2drive/tests/test_main_methods.py
@@ -1,18 +1,36 @@
 import unittest
-
+import datetime
+import tempfile
 from dropbox2drive import main
+
 
 class TestFileCompare(unittest.TestCase):
     def test(self):
-        # for testing purposes we can get one random file.
-        filename = "test_main_methods.py"
-        f1 = TestFileCompare.regular_file_to_class(filename)
-        f2 = TestFileCompare.regular_file_to_class(filename)
+        # for testing purposes we use a temporari nonsense file.
+        f = tempfile.NamedTemporaryFile()
+        f.write("Some random data inside the file\x00\x09\x48\x94\x48....")
+        f_different = tempfile.NamedTemporaryFile()
+        f_different.write("diferent data")
+
+        f1 = TestFileCompare.regular_file_to_class(f.name)
+        f2 = TestFileCompare.regular_file_to_class(f.name)
+        f3 = TestFileCompare.regular_file_to_class(f_different.name)
         self.assertEquals(f1, f2)
+        self.assertNotEquals(f1, f3)
+
+        f.close()
+        f_different.close()
 
     @staticmethod
     def regular_file_to_class(filename):
-        pass  # TODO
+        return main.File(
+            name=filename,
+            relative_path=filename,
+            cloud={'name': 'test', 'tmp_dir': 'test'},
+            original_file=filename,
+            last_modified=datetime.date.today(),
+            metadata={"some": "random metadata"},
+        )
 
 
 class TestClassifyFiles(unittest.TestCase):
