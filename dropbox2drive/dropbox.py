f52b7016d6a4e3fa9f27c58d97e6c3074dd916df
Josep <jap8@alumnes.udl.cat>
2016-10-04 11:12:35 +0300

    Added format description of the responses (as a comment)

--- dropbox2drive/dropbox.py
+++ dropbox2drive/dropbox.py
@@ -6,3 +6,89 @@ def get_files():
 def deserialize(file):
     """Convert the dropbox file format to File class."""
     pass
+
+"""
+name (String): The last component of the path (including extension). This never
+    contains a slash.
+id (String): min_length=1
+client_modified (Timestamp): (%Y-%m-%dT%H:%M:%SZ) For files, this is the
+    modification time set by the desktop client when the file was added to
+    Dropbox. Since this time is not verified (the Dropbox server stores
+    whatever the desktop client sends up), this should only be used for display
+    purposes (such as sorting) and not, for example, to determine if a file has
+    changed or not.
+server_modified (Timestamp)
+rev (String): (min_length=9, pattern="[0-9a-f]+") A unique identifier for the
+    current revision of a file. This field is the same rev as elsewhere in the
+    API and can be used to detect changes and avoid conflicts.
+size (UInt64): The file size in bytes.
+path_lower (String)?: The lowercased full path in the user's Dropbox. This
+    always starts with a slash. This field will be null if the file or folder
+    is not mounted.
+path_display (String)?: The cased path to be used for display purposes only.
+parent_shared_folder_id (String)?: (pattern="[-_0-9a-zA-Z:]+") --Deprecated--
+    Please use FileSharingInfo.parent_shared_folder_id or
+    FolderSharingInfo.parent_shared_folder_id instead.
+media_info (MediaInfo)?: Additional information if the file is a photo or
+    video.
+{
+MediaInfo (union)
+    .pending (Void): Indicate the photo/video is still under processing and
+        metadata is not available yet.
+    .metadata (MediaMetadata): The metadata for the photo/video.
+    {
+    MediaMetadata (datatype with subtypes)
+        --photo (PhotoMetadata)
+        {
+            .dimensions (Dimensions)?
+            {
+                .height (UInt64)
+                .width (UInt64)
+            }
+            .location (GpsCoordinates)?
+            {
+                .latitude (Float64)
+                .longitude (Float64)
+            }
+            .time_taken ("%Y-%m-%dT%H:%M:%SZ")?
+        }
+        --video (VideoMetadata)
+        {
+            .dimensions (Dimensions)?
+            .location (GpsCoordinates)?
+            .duration (UInt64): in milliseconds
+        }
+    }
+}
+sharing_info (FileSharingInfo)? Set if this file is contained in a shared
+    folder.
+{
+    .read_only (Boolean) True if the file or folder is inside a read-only
+        shared folder.
+    .parent_shared_folder_id (String): (pattern="[-_0-9a-zA-Z:]+") ID of shared
+        folder that holds this file.
+    .modified_by (String)?: (min_length=40, max_length=40) The last user who
+        modified the file. This field will be null if the user's account has
+        been deleted.
+}
+property_groups (List of PropertyGroup)? Additional information if the file has
+    custom properties with the property template specified.
+{
+    Collection of custom properties in filled property templates. This datatype
+    comes from an imported namespace originally defined in the properties
+    namespace.
+    .template_id (String): (min_length=1, pattern="(/|ptid:).*") A unique
+        identifier for a property template type.
+    .fields (List of PropertyField): up to 32 for a template
+    {
+        .name (String): up to 256B
+        .value (String): up to 1024B
+    }
+}
+has_explicit_shared_members (Boolean)?: This flag will only be present if
+    include_has_explicit_shared_members is true in list_folder or get_metadata.
+    If this flag is present, it will be true if this file has any explicit
+    shared members. This is different from sharing_info in that this could be
+    true in the case where a file has explicit members but is not contained
+    within a shared folder.
+"""
--- dropbox2drive/googledrive.py
+++ dropbox2drive/googledrive.py
@@ -22,3 +22,137 @@ def update_files(files):
 def serialize(file):
     """Convert the File class data to drive format."""
     pass
+
+
+"""
+https://developers.google.com/drive/v2/reference/files
+______________FILE FORMAT__________________________
+kind (string): the type of the file (Always "drive#file")
+id (string): id of the file (w)
+etag (etag): etag of the file
+selfLink (string): link back to this file
+title (string): title of the file. Used to identify file or folder name (w)
+mimeType (string): only mutable on update new content. can be blank (w)
+description (string): a hort description of the file (w)
+labels (object): (w)
+{
+    .starred (boolean): the file is starred by the user (w)
+    .hidden (boolean): --DEPRECATED (w)
+    .trashed (boolean): the file has been trashed (w)
+    .restricted (boolean): viewers are prevented from downl, print, copy. (w)
+    .viewed (boolean): the file has been seen by the user. (w)
+}
+createdDate (datetime): formatted RFC 3339 timestamp
+modifiedDate (datetime): only mutable when the param setModifiedDate=true(w)
+modifiedByMeDate (datetime): last time this user modified.
+downloadUrl (string)
+indexableText (object): (only writable)
+{
+    .text (string): text to be indexed
+}
+userPermission (nested object): permissions for the authenticated user on
+    this file
+fileExtension (string): final component of fullFileExtension.
+md5Checksum (string)
+fileSize (long)
+alternateLink (string): for opening the file in a relevant google editor or
+    viewer
+embedLink (string)
+sharedWithMeDate (datetime)
+parents[] (list): (w)
+exportLinks (map):
+{
+    .(key) (string): mapping from export format to url.
+}
+originalFilename (string): -or else the original value of the title field. (w)
+quotaBytesUsed (long)
+ownerNames[] (list)
+lastModifyingUserName (string)
+editable (boolean): by the current user
+writersCanShare (boolean): (w)
+thumbnailLink (string)
+lastViewedByMeDate (datetime)
+webContentLink (string)
+explicitlyTrashed (boolean)
+imageMediaMetadata (object): only for imagetypes, content depend on what can be
+    parsed
+{
+    .width (int)
+    .height (int)
+    .rotation (int): clockwise degrees
+    .location (object)
+        .latitude (double)
+        .longitude (double)
+        .altitude (double)
+        .date (string): EXIF format timestamp
+        .cameraModel (string)
+        .exposureTime (float)
+        .aperture (float)
+        .flashUsed (boolean)
+        .focalLength (float)
+        .isoSpeed (integer)
+        .meteringMode (string)
+        .sensor (string)
+        .exposureMode (string)
+        .colorSpace (string)
+        .whiteBalance (string)
+        .exposureBias (float)
+        .maxApertureValue (float)
+        .subjectDistance (int)
+        .lens (string)
+}
+thumbnail (object)
+{
+    .image (bytes): URL-safe Base64 encoded bytes. (conform to RFC 4648 sect 5)
+    .mimeType (string)
+}
+webViewLink (string):  	A link only available on public folders for viewing
+    their static web assets (HTML, CSS, JS, etc) via Google Drive's Website
+    Hosting.
+iconLink (string)
+shared (boolean)
+owners[] (list of users)
+lastModifyingUser (user)
+appDataContents (boolean): in the Application Data folder.
+openWithLinks (map): map for each of the user's apps to a link to open this
+    file with an app. Only populated when the drive.apps.readonly scope is used
+{
+    .(key): (string)
+}
+defaultOpenWithLink (string): link to open this file with the default app.
+    Only populated when the drive.apps.readonly scope is used
+headRevisionId (string): ID of the file's head revision. Only populated for
+    files with content stored in Drive, not G.Docs or shortcut files.
+copyable (boolean)
+properties[] (list): (w)
+markedViewedByMeDate (datetime): --DEPRECATED-- (w)
+version (long
+sharingUser (user)
+permissions[] (list)
+videoMediaMetadata (object)
+{
+    .width (int)
+    .height (int)
+    .durationMillis (long)
+}
+folderColorRgb (string): (w)
+fullFileExtension (string)
+ownedByMe (boolean)
+canComment (boolean)
+shareable (boolean)
+spaces[] (list): list of spaces which contain the file. Supported are: 'drive',
+    'appDataFolder' and 'photos'.
+canReadRevisions (boolean)
+isAppAuthorized (boolean)
+
+__________________USER FORMAT__________________________
+.kind (string): Always "drive#user"
+.displayName (string)
+.picture (object):
+{
+    .url (string)
+}
+.isAuthenticatedUser (boolean): the user is who made the request.
+.emailAddress (string)
+.permissionId (string)
+"""
